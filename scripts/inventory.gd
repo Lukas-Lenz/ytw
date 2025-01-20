c37e9668469f996309f895281ad7ca0661fc5786
Liam <dana.lenz@protonmail.com>
2025-01-20 23:28:30 +0100

    Update inventory.gd

--- scripts/inventory.gd
+++ scripts/inventory.gd
@@ -1,15 +1,40 @@
 extends Control
 
+const SIZE = 3
+
+var items : Array
+
+func _ready():
+	for i in SIZE:
+		for j in SIZE:
+			items[i][j] = null
+
+func item_at_spot(pos : Vector2i):
+	if pos.x < 0 || pos.x >= SIZE || pos.y < 0 || pos.y > SIZE:
+		return null
+	else:
+		return items[pos.x][pos.y]
+
+func add(item: Item, pos: Vector2i) -> bool:
+	
+	# position - position + dimensions must be free
+	
+	
 #func _get_drag_data(at_position: Vector2) -> Variant:
+	
+#	if get_item(at_position)
 #	pass
 	
 
-#func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
+func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
 	
-#	if !(data is Item):
-#		return false 
+	if !(data is Item):
+		return false 
 	
-#	object_dimension = data.dimensions
+	data.dimensions
+	get_placement()
+	
+	object_dimension = data.dimensions
 	
 #	pass
 	
